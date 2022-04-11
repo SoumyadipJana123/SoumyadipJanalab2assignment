855cbbb36530223290a71d9f2cf81353191eb4ca
SoumyadipJana123 <99543316+SoumyadipJana123@users.noreply.github.com>
2022-04-11 18:43:37 +0530

    Add files via upload
    Main file is for the Notescount file

---
+++ Notescount.java
@@ -0,0 +1,26 @@
+class Notes{
+public void notesCountImplementation(int notes[], int amount){
+    int[] noteCounter = new int[notes.length];
+	try{
+	   for (int i =0; i < notes.length; i++) {
+	       if (amount >= notes[i]){
+		       noteCounter[i] = amount / notes[i];
+			   amount = amount - noteCounter[i] = notes[i];
+		   }
+	   }
+	   if (amount > 0){
+	       System.out.println("exact amount cannot be given with the highest denomination")
+		  }
+		else{
+		System.out.println("Your payment approach in order to give min no of notes")
+         for(int i = 0; i < notes.length; i++){
+		     if (noteCounter[i] i= 0){
+			     System.out.println(notes[i] + "i" + noteCounter[i]);
+			 }
+		 }   
+			}  
+	}catch (ArithmeticException e){
+	   System.out.println(e+ "notes of denominaion 0 is invalid");
+	}
+}
+}
---
+++ main.java
@@ -0,0 +1,15 @@
+public class Main{
+   public static void main(String[]args){
+   MergeSortImplementation mergeSortImplementation = new MergeSortImplementation();
+   NotesCount notesCount = new NotesCount();
+   
+   System.out.println("enter the currency denomination value");
+   for (int i = 0; i < size; i++){
+        notes[i] = sc.nextInt();
+   }
+   System.out.println("enter the amount you want to pay");
+   int amount = sc.nextInt();
+   mergeSortImplementation.sort(notes,0,notes.length-1);
+   notesCount.notesCountImplementation(notes,amount);
+   }
+}
